b251963c3126b857f35d1976f3090d43fc5b851b
malboushi <mohammad.alaa.love.619@gmail.com>
2026-07-08 23:24:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ const messaging = firebase.messaging();
 // لأن فايربيز يقوم بعرض الإشعارات تلقائياً بناءً على البيانات المرسلة من ملف send.js
 
 // رفعنا الإصدار ليجبر المتصفح ياخد النسخة الجديدة
-const CACHE_NAME = 'app-cache-32'; 
+const CACHE_NAME = 'app-cache-33'; 
 const ASSETS = [
   './',
   './index.html',
