8b8df6ebe2c2217e76e5d80a670a9adc29b885c6
malboushi <mohammad.alaa.love.619@gmail.com>
2026-09-20 18:39:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(function(payload) {
 });
 
 // رفعنا الإصدار ليجبر المتصفح ياخد النسخة الجديدة
-const CACHE_NAME = 'app-cache-14'; 
+const CACHE_NAME = 'app-cache-15'; 
 const ASSETS = [
   './',
   './index.html',
